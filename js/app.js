c04ec6d364dc371b5c6dd77645989901d5877839
vagnqvistgaard <U773554@rts-365.dk>
2021-09-09 14:19:18 +0200

    app.js m. navne, adresser m.m.

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// json-opgaver for da world
+
+const jsonObjects = {
+    team: [
+
+        {
+            "fornavn": "Anders",
+            "efternavn": "And",
+            "alder": 90,
+            "adresse": "Paradisæblevej, Andeby"
+        },
+
+        {
+            "fornavn": "Rip",
+            "efternavn": "And",
+            "alder": 35,
+            "adresse": "Paradisæblevej, Andeby"
+        },
+        {
+            "fornavn": "Rap",
+            "efternavn": "And",
+            "alder": 36,
+            "adresse": "Paradisæblevej, Andeby"
+
+        },
+        {
+            "fornavn": "Rup",
+            "efternavn": "And",
+            "alder": 37,
+            "adresse": "Paradisæblevej, Andeby"
+
+        },
+        {
+            "fornavn": "Andersine",
+            "efternavn": "And",
+            "alder": 60,
+            "adresse": "Andeby Hovedgade, Andeby"
+        }
+    ]
+}
+
+
+
